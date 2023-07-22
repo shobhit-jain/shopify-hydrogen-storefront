efdc8cbd092093c70845409895340820f2c2d3fb
shobhit-jain <shobhitjain.work@gmail.com>
2023-07-22 19:58:31 +0530

    initial-commit

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -2,7 +2,43 @@
 /// <reference types="@shopify/remix-oxygen" />
 /// <reference types="@shopify/oxygen-workers-types" />
 
-import type {WithCache} from '@shopify/hydrogen';
+// import type {WithCache} from '@shopify/hydrogen';
+// import type {Storefront} from '~/lib/type';
+// import type {HydrogenSession} from '~/lib/session.server';
+
+// declare global {
+//   /**
+//    * A global `process` object is only available during build to access NODE_ENV.
+//    */
+//   const process: {env: {NODE_ENV: 'production' | 'development'}};
+
+//   /**
+//    * Declare expected Env parameter in fetch handler.
+//    */
+//   interface Env {
+//     SESSION_SECRET: string;
+//     PUBLIC_STOREFRONT_API_TOKEN: string;
+//     PRIVATE_STOREFRONT_API_TOKEN: string;
+//     PUBLIC_STORE_DOMAIN: string;
+//     PUBLIC_STOREFRONT_ID: string;
+//   }
+// }
+
+// /**
+//  * Declare local additions to `AppLoadContext` to include the session utilities we injected in `server.ts`.
+//  */
+// declare module '@shopify/remix-oxygen' {
+//   export interface AppLoadContext {
+//     waitUntil: ExecutionContext['waitUntil'];
+//     session: HydrogenSession;
+//     storefront: Storefront;
+//     env: Env;
+//   }
+// }
+
+// // Needed to make this file a module.
+// export {};
+
 import type {Storefront} from '~/lib/type';
 import type {HydrogenSession} from '~/lib/session.server';
 
@@ -10,7 +46,7 @@ declare global {
   /**
    * A global `process` object is only available during build to access NODE_ENV.
    */
-  const process: {env: {NODE_ENV: 'production' | 'development'}};
+  const process: {env: {NODE_ENV: 'production' | 'development'} & Env};
 
   /**
    * Declare expected Env parameter in fetch handler.
